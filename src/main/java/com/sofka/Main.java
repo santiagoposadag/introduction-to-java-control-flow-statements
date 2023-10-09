208889121d32551b967450dd0f30cbaa165e47d7
Santiago Posada <santiago90dell@gmail.com>
2023-10-09 17:43:53 -0500

    switch statement ready

--- src/main/java/com/sofka/Main.java
+++ src/main/java/com/sofka/Main.java
@@ -10,14 +10,53 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        //Un estudiante que está en
-        // una clase no puede estar en otra clase al mismo
-        // tiempo y esto lo identificamos con el nombre completo.
+        //Un estudiante que está en una clase
+        //no puede estar en otra clase al mismo tiempo
+        //y esto lo identificamos con el nombre completo (Nombre y apellido).
 
-        String menu = "1-Añadir estudiantes\n" +
-                "2-Eliminar estudiantes\n" +
-                "3-Buscar estudiantes dentro de una clase\n" +
-                "4-Buscar todos los estudiantes que tienen el mismo nombre.\n";
+        String menu = "1-Crear clase\n" +
+                "2-Añadir estudiante a clase" +
+                "3-Eliminar estudiantes de una clase\n" +
+                "4-Buscar estudiantes dentro de una clase\n" +
+                "5-Buscar todos los estudiantes que tienen el mismo nombre.\n" +
+                "6-Finalizar ejecución";
+
+        boolean doContinue = true;
+
+        while (doContinue){
+            System.out.print("¿Cual acción quieres realizar? }¿\n" +
+                    "escribe el número de la acción: ");
+
+            int chosenOption = scanner.nextInt();
+
+            switch (chosenOption){
+                case 1:
+                    System.out.println("Crear clase");
+                    break;
+                case 2:
+                    System.out.println("Añadir estudiante a clase");
+                    break;
+                case 3:
+                    System.out.println("Eliminar estudiante de una clase");
+                    break;
+                case 4:
+                    System.out.println("Buscar estudiante dentro de una clase");
+                    break;
+                case 5:
+                    System.out.println("Buscar estudiantes que tienen el mismo nombre");
+                    break;
+                case 6:
+                    System.out.println("Finalizar ejecución");
+                    doContinue = false;
+                    break;
+                default:
+                    System.out.println("La opción escogida no es valida\n" +
+                            "Selecciona otra:");
+                    break;
+            }
+        }
+
+
 
         scanner.close();
     }
